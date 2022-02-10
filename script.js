8b416580d701c066c0ba720bd0df5ceeb9b8f6d2
DaHbka <flexinzflexinz@gmail.com>
2022-02-10 19:10:38 +0300

    Выполнено задание 11

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
+const button = document.querySelector('#btn');
+const input = document.querySelector('input[type="text"]');
+const square = document.querySelector('#square');
 
+const circle = square.querySelector('#circle');
+const circleButton = circle.querySelector('#e_btn');
 
+const rangeInput = document.querySelector('input[type="range"]');
 
+// 1
+button.addEventListener('click', () => {
+    square.style.backgroundColor = input.value;
+})
 
+// 2
+circleButton.style.display = 'none';
 
+// 3
+rangeInput.addEventListener('input', (event) => {
+    circle.style.width = event.target.value + '%';
+    circle.style.height = event.target.value + '%';
+})
